2b47d115efd17d2e4d914c1c407b759ce0378728
lsimonl <54697119+lsimonl@users.noreply.github.com>
2020-03-05 21:35:32 +0000

    dropout_layers

---
+++ dropout_layer.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+
+
+
+def layer (inputs, insize, outsize,keep_prob = 1, activation = False):
+    
+    weights = tf.Variable(tf.random_normal([insize,outsize])) #权重值设定，随机且符合正态分布，
+    #insize代表 input的一个张量 outsize代表输出的张量，也是神经元个数
+    biases = tf.Variable(tf.zeros([1,outsize]) +0.1)
+    #偏置的张量为一行但有很多列, 0.1 为noise
+    
+    wx_plus_b = tf.matmul(tf.cast(inputs,tf.float32),weights) + biases #将input转为float32
+    wx_plus_b = tf.nn.dropout(wx_plus_b, keep_prob)
+    if activation is False:
+        outputs = wx_plus_b
+        
+    else:
+        outputs = activation(wx_plus_b)
+    return outputs
+    
