bfbd6315ba63b6aec6fe984653845706b5b84e13
lsimonl <54697119+lsimonl@users.noreply.github.com>
2020-03-17 21:28:06 +0000

    Update sk_crossValidation.py

--- sk_crossValidation.py
+++ sk_crossValidation.py
@@ -2,7 +2,7 @@
 from sklearn.model_selection import train_test_split
 
 
-def cross_validation(x_data, y_data):
+def cross_validation(x_dataValue, y_dataValue):
     x_train, x_test, y_train, y_test = train_test_split(
-            x_data,y_data,test_size=0.3)
+            x_dataValue,y_dataValue,test_size=0.3)
     return x_train,x_test,y_train,y_test
