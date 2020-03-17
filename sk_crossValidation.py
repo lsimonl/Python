5dede738ac51ca7d0e790daa864688ef50ce48a2
lsimonl <54697119+lsimonl@users.noreply.github.com>
2020-03-17 23:46:27 +0000

    Delete sk_crossValidation.py

--- sk_crossValidation.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from sklearn.model_selection import train_test_split
-
-
-def cross_validation(x_dataValue, y_dataValue):
-    x_train, x_test, y_train, y_test = train_test_split(
-            x_dataValue,y_dataValue,test_size=0.3)
-    return x_train,x_test,y_train,y_test
